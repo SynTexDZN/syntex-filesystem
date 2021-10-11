df5ad7ce43454aa04a3daa0e4fc46c8f85032665
SynTexDZN <syntexdzn@t-online.de>
2021-10-11 20:43:13 +0200

    Init Commit

---
+++ main.js
@@ -0,0 +1,149 @@
+const fs = require('fs'), path = require('path');
+
+var ready = false, cache = {};
+
+module.exports = class FileManager
+{
+	constructor(basePath, logger, initDirectories)
+	{
+		this.logger = logger;
+
+		if(basePath != null)
+		{
+			try
+			{
+				var absPath = path.resolve(basePath);
+
+				fs.accessSync(absPath, fs.constants.W_OK);
+
+				this.basePath = absPath;
+
+				if(initDirectories != null)
+				{
+					this.createDirectories(initDirectories);
+				}
+
+				ready = true;
+			}
+			catch(e)
+			{
+				this.logger.err(e);
+			}
+		}
+		else
+		{
+			this.logger.log('error', 'bridge', 'Bridge', 'Es wurde kein Pfad für die Datensicherung angegeben!');
+		}
+	}
+
+	isReady()
+	{
+		return ready;
+	}
+
+	createDirectories(directories)
+	{
+		if(this.isReady())
+		{
+			for(const i in directories)
+			{
+				var absPath = path.join(this.basePath, directories[i]);
+
+				if(!fs.existsSync(absPath))
+				{
+					fs.mkdirSync(absPath);
+				}
+			}
+		}
+	}
+
+	readFile(relPath)
+	{
+		return new Promise((resolve) => {
+
+			if(this.isReady() && relPath != null)
+			{
+				var filePath = path.join(this.basePath, relPath);
+
+				fs.readFile(filePath, (err, file) => {
+
+					if(file && !err)
+					{
+						try
+						{
+							file = file.toString();
+
+							if(path.parse(filePath).ext == '.json')
+							{
+								file = JSON.parse(file);
+							}
+
+							cache[filePath] = JSON.stringify(file);
+
+							resolve(file);
+						}
+						catch(e)
+						{
+							resolve(null);
+						}
+					}
+					else
+					{
+						resolve(null);
+					}
+				});
+			}
+			else
+			{
+				resolve(null);
+			}
+		});
+	}
+
+	writeFile(relPath, data)
+	{
+		return new Promise((resolve) => {
+
+			if(this.isReady() && relPath != null)
+			{
+				var filePath = path.join(this.basePath, relPath);
+
+				if(this.fileChanged(filePath, data))
+				{
+					if(path.parse(filePath).ext == '.json')
+					{
+						data = JSON.stringify(data, null, '\t');
+					}
+
+					fs.writeFile(filePath, data, (err) => {
+						
+						if(!err)
+						{
+							cache[filePath] = JSON.stringify(data);
+						}
+
+						resolve(err == null);
+					});
+				}
+				else
+				{
+					resolve(false);
+				}
+			}
+			else
+			{
+				resolve(false);
+			}
+		});
+	}
+
+	fileChanged(filePath, data)
+	{
+		if(cache[filePath] != null && cache[filePath] == JSON.stringify(data))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+}
